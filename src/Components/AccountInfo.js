012418def7d63c4d94d1979f5b5b5380f1804f11
RAINEEN20 <raineen20@gcc.edu>
2023-04-25 11:08:29 -0400

    New Account Info

--- src/Components/AccountInfo.js
+++ src/Components/AccountInfo.js
@@ -1,16 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "../Styling/AccountInfoStyles.css";
+import axios from "axios";
 
 class AccountInfo extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      username: ""
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/api/accountInfo')
+        .then(accountI => {
+          this.setState({
+            name: accountI.data[0],
+            username: accountI.data[1]
+          })
+        })
+        .catch(error => {
+          console.log(error)
+        })
+  }
+
   render() {
+    const{name, username} = this.state
     return (
         <div className = "account">
           <h2>Account Information</h2>
           <div className = "fields"></div>
-            <p>Username: emilynraine</p>
-            <p>Email: raineen20@gcc.edu</p>
-            <p>First Name: Emily</p>
-            <p>Last Name: Raine</p>
+            <p>Name: {name}</p>
+            <p>Username: {username}</p>
           <div/>
         </div>
       );
