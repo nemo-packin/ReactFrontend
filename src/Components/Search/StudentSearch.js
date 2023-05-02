b3e57e6ae4a2017e25ff5884d1a00f695d9f84f6
JUDGEPM20 <pjudge2002@gmail.com>
2023-05-02 11:15:53 -0400

    working on student search

---
+++ src/Components/Search/StudentSearch.js
@@ -0,0 +1,19 @@
+import { React } from 'react'
+
+const StudentSearch = () => {
+
+    return (
+        <>
+            <input
+                className='text-black border-solid border-2 border-grey-light'
+                type='search'
+                placeholder='Student Search'
+                onChange={(e) => {
+                   
+                }}
+            />
+        </>
+    )
+}
+
+export default StudentSearch 
